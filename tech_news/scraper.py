b64777ef605003e88102c73c3988c49f347de037
Alan Foster <alan-af@outlook.com>
2023-05-16 16:08:27 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,9 +30,7 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     select = Selector(text=html_content)
-    link = select.css(
-        "div[class=nav-links] a:last-of-type::attr(href)"
-    ).get()
+    link = select.css("div[class=nav-links] a:last-of-type::attr(href)").get()
     if not link:
         return None
     return link
@@ -40,7 +38,33 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    select = Selector(text=html_content)
+    url = select.css("head link[rel=canonical]::attr(href)").get()
+    title = select.css(".entry-title::text").get()
+    if title is None:
+        title = select.css(".Hero_hero__title__dCXAM-title::text").get()
+    title = title.strip()
+    timestamp = select.css(".meta-date::text").get()
+    writer = select.css(".author > a::text").get()
+    reading_time = int(
+        select.css(".meta-reading-time::text").get().split(" ")[0]
+    )
+    summary = (
+        select.css("div[class=entry-content] > p:first-of-type")
+        .xpath("string()")
+        .get()
+        .strip()
+    )
+    category = select.css(".category-style .label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
