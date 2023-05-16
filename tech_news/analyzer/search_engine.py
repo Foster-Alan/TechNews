01cd2a1b1c0a850c36011bf843aa9cf0d87e9c39
Alan Foster <alan-af@outlook.com>
2023-05-16 16:51:11 -0300

    req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    new = search_news({"title": {"$regex": title, "$options": "i"}})
+    if new:
+        return [(i["title"], i["url"]) for i in new]
+    return []
 
 
 # Requisito 8
